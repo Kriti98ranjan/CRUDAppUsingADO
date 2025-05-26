1585b70f1749bac9a0d78ddde4613c720a7dd875
Kriti Ranjan <121679736+Kriti98ranjan@users.noreply.github.com>
2025-05-26 22:10:40 +0530

    Update ConnectionString.cs

--- CRUDAppUsingADO/ConnectionString.cs
+++ CRUDAppUsingADO/ConnectionString.cs
@@ -3,7 +3,7 @@
     public static class ConnectionString
     {
         //Create a private connection string variables :-cs
-        private static string cs = "Server = KRITI\\SQLEXPRESS; Database = CrudADOdb; Trusted_Connection = True; TrustServerCertificate=True;";
+        private static string cs = "Server = ; Database = ; Trusted_Connection = True; TrustServerCertificate=True;";
 
         // create a string for get the private connection...
         public static string dbcs { get => cs; }
